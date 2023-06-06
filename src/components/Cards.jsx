bd0a616aaacfd05aafb10eff32b546497a8a634d
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-06-06 16:15:53 -0300

    ejercicio con un filtro funcionando

---
+++ src/components/Cards.jsx
@@ -0,0 +1,19 @@
+import { Button, Card, Col } from "react-bootstrap";
+
+const Cards = ({ objetoNoticia }) => {
+  return (
+    <Col md="5" lg="4">
+      <Card className="shadow h-100">
+        <Card.Img variant="top" src={objetoNoticia.image_url}></Card.Img>
+        <Card.Body>
+          <Card.Title>{objetoNoticia.title}</Card.Title>
+        </Card.Body>
+        <Card.Footer className="text-end">
+          <Button variant="danger">Ver noticia completa</Button>
+        </Card.Footer>
+      </Card>
+    </Col>
+  );
+};
+
+export default Cards;
---
+++ src/components/RowCards.jsx
@@ -0,0 +1,13 @@
+import { Row } from "react-bootstrap";
+import Cards from "./Cards";
+const RowCards = ({ noticiasFiltradas }) => {
+  return (
+    <Row className="gy-3 justify-content-center">
+      {noticiasFiltradas.map((objetoNoticia) => (
+        <Cards objetoNoticia={objetoNoticia}></Cards>
+      ))}
+    </Row>
+  );
+};
+
+export default RowCards;
