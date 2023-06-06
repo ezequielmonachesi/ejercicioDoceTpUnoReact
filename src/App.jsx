884faa90f1dfa77db46f76c396437d022826f87f
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-06-06 16:00:58 -0300

    se realiza ejercicio cargar noticia con un filtro

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Container } from "react-bootstrap";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AdministradorNoticias from "./components/AdministradorNoticias";
+import Titulo from "./components/Titulo";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <></>
-  )
+    <>
+      <Container className="py-3 px-md-5 main">
+        <Titulo></Titulo>
+        <AdministradorNoticias></AdministradorNoticias>
+      </Container>
+      <footer className="py-4 bg-black text-light text-center">
+        &copy; Todos los derechos reservados 2023.
+      </footer>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/AdministradorNoticias.jsx
@@ -0,0 +1,65 @@
+import { Card, Form, Row, Col } from "react-bootstrap";
+import RowCards from "./RowCards";
+import { useState, useEffect } from "react";
+
+const AdministradorNoticias = () => {
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState("");
+  const [noticias, setNoticias] = useState([]);
+  const [noticiasFiltradas, setNoticiasFiltradas] = useState([]);
+
+  useEffect(() => {
+    consultarAPI();
+  }, [categoriaSeleccionada]);
+
+  const consultarAPI = async () => {
+    try {
+        const respuesta = await fetch("https://newsdata.io/api/1/news?apikey=pub_240135ddcbf2e44d1a628028e9bb6a82d03a4&q=ukraine");
+        const informacion = await respuesta.json();
+        console.log(respuesta);
+        setNoticias(informacion.results);
+        console.log(noticias);
+        const nFiltradas = noticias.filter((e)=>e.category[0] === categoriaSeleccionada)
+        setNoticiasFiltradas(nFiltradas);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <Card className="mt-4 shadow">
+        <Card.Header className="pt-3">
+          <Form>
+            <Form.Group
+              as={Row}
+              className="mb-3 justify-content-center"
+              controlId="formPlaintextEmail"
+            >
+              <Form.Label column sm="2" md="4">
+                Search by category:
+              </Form.Label>
+              <Col sm="10" md="6" className="px-2 px-md-4">
+                <Form.Select
+                  aria-label="categorías"
+                  onChange={(e) => {
+                    setCategoriaSeleccionada(e.target.value);
+                  }}
+                >
+                  <option value="all">select by category</option>
+                  <option value="top">Mundial</option>
+                  <option value="business">Business</option>
+                  <option value="politics">Politics</option>
+                </Form.Select>
+              </Col>
+            </Form.Group>
+          </Form>
+        </Card.Header>
+        <Card.Body>
+          <RowCards noticiasFiltradas={noticiasFiltradas}></RowCards>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default AdministradorNoticias;
